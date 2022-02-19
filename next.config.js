aa9b5d752fa9387ebe85f5cc05f02ee76d055e04
to-na <tonanatz@gmail.com>
2022-02-20 03:13:41 +0900

    compiled survey

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 // next.config.js
-const withLinaria = require('next-linaria');
+const { CompiledExtractPlugin } = require("@compiled/webpack-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const withLinaria = require("next-linaria");
 module.exports = withLinaria({
-  webpack(config, options) {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      use: [
+        {
+          loader: "@compiled/webpack-loader",
+          options: {
+            extract: true,
+          },
+        },
+      ],
+    });
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, "css-loader"],
+    });
+    config.plugins.push(new MiniCssExtractPlugin());
+    config.plugins.push(new CompiledExtractPlugin());
     return config;
   },
 });
